f0c64103cb2508b19ffc2c060f9d405d15b428d8
HLQ <lequanghung98@gmail.com>
2020-05-16 01:23:51 +0700

    Add react-native-fs

--- Misc/index.js
+++ Misc/index.js
@@ -3,7 +3,7 @@ import {View, Text} from 'react-native';
 export default function App() {
     return (
         <View>
-            <Text>This is the App restart</Text>
+            <Text>This is the App again</Text>
         </View>
     );
 }
